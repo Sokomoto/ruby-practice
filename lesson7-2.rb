7e16cbd0d828634d8a174fe30869271d69e85f70
Sokomoto <wdws2342319@gmail.com>
2020-11-26 14:09:36 +0900

    演習7-2.1

--- lesson7-2.rb
+++ lesson7-2.rb
@@ -1,13 +1,13 @@
 puts "計算をはじめます"
  
-    puts "2つの値を入力してください"
+  puts "何回繰り返しますか？"
  
-    a = gets.to_i
+  input = gets.to_i
  
-    b = gets.to_i
+  i = 1 
  
-    puts "計算結果を出力します"
+  while i <= input do 
  
-    puts "a*b=#{a * b}"
+      puts "#{i}回目の計算" 
  
-    puts "計算を終了します"
+      puts "2つの値を入力してください" 
